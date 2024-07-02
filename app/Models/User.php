eef7daec916a67606a5a1f653279b65fae525198
RjHamid <hamidj1383@gmail.com>
2024-07-02 15:30:41 +0330

    correcting password names

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
+        'Password',
     ];
 
     /**
@@ -38,6 +38,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'password' => 'hashed',
+        'Password' => 'hashed',
     ];
 }
