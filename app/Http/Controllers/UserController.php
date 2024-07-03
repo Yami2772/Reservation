f316bd3d8eadb8f780c3caf54a7343a045ec5f0b
hesam <hesamedin.a.2772@gmail.com>
2024-07-03 11:00:01 -0700

    cleaning usercontroller's codes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,22 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index($id= null){
-        $user= User::all();
-        if($id) {
-            $user=User::where('id',$id)->first();
+    public function index($id = null)
+    {
+        if ($id) {
+            $User = User::where('id', $id)->first();
         }
-    
-        return response()->json($user);
+
+        return response()->json($User);
     }
-    
-    public function edit(Request $request, $id){
-    $user=User::where('id',$id)->update($request->toArray());
+
+    public function edit(Request $request, $id)
+    {
+        $User = User::where('id', $id)
+            ->update($request
+                ->merge(["Password" => Hash::make($request->Password)])
+                ->toArray());
+
+        return response()->json($User);
     }
-    public function delete($id){
-    $user=User::where('id',$id)->delete();
+
+    public function delete($id)
+    {
+        User::where('id', $id)->delete();
+
+        return response()->json('User deleted successfully!');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,17 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::group(['prefix'=> 'user','as'=>'user.'] ,function(){
-    Route::get('index/{id?}',[UserController::class,'index'])->name('index');
-    Route::put('edit/{id}',[UserController::class,'edit'])->name('edit');
-    Route::delete('delete/{id}',[UserController::class, 'delete'])->name('delete');
-});
+// Route::group(['prefix'=> 'user','as'=>'user.'] ,function(){
+//     Route::get('index/{id?}',[UserController::class,'index'])->name('index');
+//     Route::put('edit/{id}',[UserController::class,'edit'])->name('edit');
+//     Route::delete('delete/{id}',[UserController::class, 'delete'])->name('delete');
+// });
 
+Route::prefix('users')->middleware('auth:sanctum')->group(function () {
+    //users_list
+    Route::get('index', UserController::class, 'index')->name('index');
+    //users_edit
+    Route::put('edit', UserController::class, 'edit')->name('edit');
+    //users_delete
+    Route::delete('delete', UserController::class, 'delete')->name('delete');
+});
