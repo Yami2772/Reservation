23f479a74bfdbf2ffd74d38aefab5cdd6a47089d
hesam <hesamedin.a.2772@gmail.com>
2024-07-04 15:18:59 -0700

    correcting auth routes

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     //register
     Route::post('register', [AuthController::class, 'register'])->name('register');
-    //login(normal)
+    //login
     Route::post('login', [AuthController::class, 'logiN'])->name('login');
-    //login(code_request)
-
-    //login(code_confirm)
-
 });
 
 //users
