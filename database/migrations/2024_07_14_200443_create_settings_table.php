f17b628d40a15f220e56b05e86b1c69995095c69
hesam <hesamedin.a.2772@gmail.com>
2024-07-14 13:10:29 -0700

    definig tables and fillable parts in setting migration and model

--- database/migrations/2024_07_14_200443_create_settings_table.php
+++ database/migrations/2024_07_14_200443_create_settings_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
+            $table->string('key');
+            $table->string('value');
+            $table->enum('type', ['img', 'string']);
             $table->timestamps();
         });
     }
