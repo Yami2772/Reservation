fe1ab219ff735c7237e6aca6aeaff42499058ebb
hesam <hesamedin.a.2772@gmail.com>
2024-07-18 15:21:03 -0700

    creating and completing create user request

---
+++ app/Http/Requests/CreateUserRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|max:12',
+            'last_name' => 'required|max:18',
+            'phone_number' => 'required|digits:11|numeric',
+            'password' => 'required|min:8'
+        ];
+    }
+}
