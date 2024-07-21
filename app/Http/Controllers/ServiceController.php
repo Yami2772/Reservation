dcc5f54b8c4a38d19fce1a29a7e265773d064e90
hesam <hesamedin.a.2772@gmail.com>
2024-07-21 14:11:51 -0700

    creating and completing ServiceController's CRUD

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    public function create(Request $request)
+    {
+        $Service = Service::create($request->toArray());
+
+        return response()->json($Service);
+    }
+
+    public function read($id = null)
+    {
+        if ($id) {
+            $Service = Service::where('id', $id)->first();
+        } else {
+            $Service = Service::paginate(5);
+        }
+
+        return response()->json($Service);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $Service = Service::where('id', $id)->update($request->toArray());
+
+        return response()->json($Service);
+    }
+
+    public function delete($id)
+    {
+        Service::where('id', $id)->delete();
+
+        return response()->json('Service deleted successfully!');
+    }
+}
