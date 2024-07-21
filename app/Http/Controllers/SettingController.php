8b0ccb97f98a2dbad0f1a97466da99cb2f2483b7
hesam <hesamedin.a.2772@gmail.com>
2024-07-21 14:50:51 -0700

    creating and completing SettingController's CRUD

---
+++ app/Http/Controllers/SettingController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Setting;
+use Illuminate\Http\Request;
+
+class SettingController extends Controller
+{
+    public function create(Request $request)
+    {
+        $Setting = Setting::create($request->toArray());
+
+        return response()->json($Setting);
+    }
+
+    public function read(Request $request)
+    {
+        $key = $request->key;
+        if ($key) {
+            $Setting = Setting::where('key', $key)->first();
+        } else {
+            $Setting = Setting::paginate(5);
+        }
+
+        return response()->json($Setting);
+    }
+
+    public function update(Request $request)
+    {
+        $key = $request->key;
+        $Setting = Setting::where('key', $key)->update($request->toArray());
+
+        return response()->json($Setting);
+    }
+
+    public function delete(Request $request)
+    {
+        $key = $request->key;
+        Setting::where('key', $key)->delete();
+
+        return response()->json('Setting deleted successfully!');
+    }
+}
