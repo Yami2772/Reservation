4d856df7e8de4690d366ef0115e8b96b331a9cb7
hesam <hesamedin.a.2772@gmail.com>
2024-07-29 14:27:03 -0700

    completing timing controller

--- app/Http/Controllers/TimingController.php
+++ app/Http/Controllers/TimingController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Timing;
 use Illuminate\Http\Request;
 
 class TimingController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $timing = Timing::create($request->toArray());
+
+        return response()->json($timing);
+    }
+
+    public function read()
+    {
+        $timing = Timing::get();
+
+        return response()->json($timing);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $timing = Timing::where('id', $id);
+        if (!$timing) {
+            return response()->json('Timing not found!');
+        } else {
+            $timing->update($request->toArray());
+        }
+
+        return response()->json($timing);
+    }
+
+    public function delete($id)
+    {
+        $timing = Timing::where('id', $id);
+        if ($timing) {
+            $timing->delete();
+            return response()->json('Timing deleted successfully!');
+        } else {
+            return response()->json('Timing not found!');
+        }
+    }
 }
