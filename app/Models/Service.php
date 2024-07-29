32803f2664e3ec924fee730681965b15496a2519
hesam <hesamedin.a.2772@gmail.com>
2024-07-29 09:31:25 -0700

    defining many-to-many relation of service_timing table

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Service extends Model
@@ -34,5 +35,10 @@ class Service extends Model
     public function reservations(): HasMany
     {
         return $this->hasMany(Reservation::class);
+    }
+
+    public function timings(): BelongsToMany
+    {
+        return $this->belongsToMany(Timing::class);
     }
 }
