a56a7cacbbe958a049927633060f4f46603741b4
hesam <hesamedin.a.2772@gmail.com>
2024-08-08 13:28:09 -0700

    creating user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'full_name' => fake()->name(),
+            'phone_number' => fake()->phoneNumber(),
+            'sex' => fake()->randomElement('male', 'female'),
+            'national_code' => fake()->random_int(10),
+            'birth_date' => fake()->date(),
+            'password' => Hash::make('password'),
         ];
     }
 
