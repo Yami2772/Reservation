7b7d008100011623ecd19af1fd4977dbf7800a2f
hesam <hesamedin.a.2772@gmail.com>
2024-08-11 15:49:47 -0700

    creating reservation_factory

---
+++ database/factories/ReservationFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
+ */
+class ReservationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'service_id' => fake()->numberBetween(1,4),
+            'timing_id' => fake()->numberBetween(1,6),
+            'user_id' => fake()->randomDigitNotZero(),
+            'date' => fake()->date()
+        ];
+    }
+}
