32ddb3a43f862a747b075d934b18678cade7b3b8
hesam <hesamedin.a.2772@gmail.com>
2024-09-15 17:11:51 -0700

    adding reservation status in resevation request

--- app/Http/Requests/CreateAndUpdateReservationRequest.php
+++ app/Http/Requests/CreateAndUpdateReservationRequest.php
@@ -28,7 +28,7 @@ class CreateAndUpdateReservationRequest extends FormRequest
             'timing_id' => 'required|digits_between:1,6',
             'user_id' => 'required|digits_between:1,1000',
             'date' => 'required|date_format:Y-m-d',
-            'status' => ['required', new Enum(ReservationStatus::class)],
+            'status' => ['nullable', new Enum(ReservationStatus::class)],
         ];
     }
 }
